c90c2b8e084ef98b05602fb7eab23c75a371965a
Ezerodriguez18 <ezepr1804@gmail.com>
2025-12-02 15:39:35 -0300

    Graficos.py

--- Graficos.py
+++ Graficos.py
@@ -6,12 +6,17 @@ import matplotlib.pyplot as plt
 
 class Canvas_graficatiempo(FigureCanvas):
     def __init__(self, parent=None):
-        self.fig, self.ax = plt.subplots(facecolor='gray')
+        self.fig, self.ax = plt.subplots(facecolor='white')
         super().__init__(self.fig)
         self.ax.grid(True)
         self.ax.margins(x=0)
         self.ax.axhline(0, color='black', linewidth=1)  
         self.ax.axvline(0, color='black', linewidth=1)  
+        self.ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+        self.ax.xaxis.get_offset_text().set_fontsize(10)  # Tamaño del texto "x10^"
+        self.ax.set_title("Gráfica en el dominio del tiempo")
+        self.ax.set_xlabel("Tiempo (s)")
+        self.ax.set_ylabel("Amplitud")
 
     def plot(self,t,x):
         self.ax.clear()
@@ -33,14 +38,19 @@ class Canvas_graficatiempo(FigureCanvas):
 
 class Canvas_graficafrecuencia(FigureCanvas):
     def __init__(self, parent=None):
-        self.fig, self.ax = plt.subplots(facecolor='gray')
+        self.fig, self.ax = plt.subplots(facecolor='white')
         super().__init__(self.fig)
         self.ax.grid(True)
         self.ax.margins(x=0)
         self.ax.axhline(0, color='black', linewidth=1)  
-        self.ax.axvline(0, color='black', linewidth=1)  
+        self.ax.axvline(0, color='black', linewidth=1) 
+        self.ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
+        self.ax.xaxis.get_offset_text().set_fontsize(10)  # Tamaño del texto "x10^" 
+        self.ax.set_title("Gráfica en el dominio de la frecuencia")
+        self.ax.set_xlabel("Frecuencia (Hz)")
+        self.ax.set_ylabel("Magnitud")
     
-    def plot(self,frecuencias,magnitude,findice,famp,spurioindice,spurioamp,diezmado,longitud):
+    def plot(self,frecuencias,magnitude,findice,famp,spurioindice,spurioamp,diezmado,longitud,sfdrvalue):
         self.ax.clear()
         self.ax.grid(True)
         self.ax.margins(x=0)
@@ -50,11 +60,11 @@ class Canvas_graficafrecuencia(FigureCanvas):
         self.ax.plot(frecuencias, magnitude) # grafica fft
         self.ax.ticklabel_format(axis='x', style='sci', scilimits=(0, 0))
         self.ax.xaxis.get_offset_text().set_fontsize(10)  # Tamaño del texto "x10^"
-        plt.fill_between((0,60000000/(diezmado*2)),(spurioamp,spurioamp),(famp,famp), label = 'SFDR',
+        plt.fill_between((0,60000000/(diezmado*2)),(spurioamp,spurioamp),(famp,famp), label=f'SFDR = {sfdrvalue:.2f} dB',
                      color = "lightblue")  
-        self.ax.plot(findice, famp, marker="s", label=f'fundamental =  {famp:.2f}')
-        self.ax.plot(spurioindice, spurioamp, marker="s",label =f'spurs =  {spurioamp:.2f} ')
-        self.ax.legend()
+        self.ax.plot(findice, famp, marker="s", label=f'fundamental =  {famp:.2f} dB')
+        self.ax.plot(spurioindice, spurioamp, marker="s",label =f'spurs =  {spurioamp:.2f} dB')
+        self.ax.legend().set_draggable(True)
         self.ax.set_title("Gráfica en el dominio de la frecuencia")
         self.ax.set_xlabel("Frecuencia (Hz)")
         self.ax.set_ylabel("Magnitud")
